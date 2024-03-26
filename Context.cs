64d54a49655c7962fb3c131cec468ad796633a9f
cedricktshibangu <tshishi@outlook.be>
2024-03-26 09:25:42 +0100

    MySql.EntityFramwork works

--- Context.cs
+++ Context.cs
@@ -2,19 +2,27 @@ using System.Data.Common;
 using Chameleon.DataAccess.Entity;
 using Chameleon.DataAccess.EntityConfig;
 using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 
 namespace Chameleon;
 
-public class Context : DbContext
+public class Context : Microsoft.EntityFrameworkCore.DbContext
 {
     public DbSet<User> User { get; set; }
     public DbSet<Roles> Roles { get; set; }
     public DbSet<UsersRoles> UsersRoles { get; set; }
 
+    
+    
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseMySQL("server=localhost;database=Chameleons;user=root;password=tshishi");
+        optionsBuilder.UseMySql(
+            "server=localhost;user id=root;password=tshishi;database=Chameleons",
+            new MariaDbServerVersion(new Version(10, 5, 4)),
+            mySqlOptions => {}
+        );
     }
+
 
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
--- Program.cs
+++ Program.cs
@@ -19,6 +19,8 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
