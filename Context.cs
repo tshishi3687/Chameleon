371c8b4a130d3b44775424b47d28c3178d17318a
cedricktshibangu <tshishi@outlook.be>
2024-03-27 12:26:34 +0100

    Add LocalityServiceTest.cs

--- Context.cs
+++ Context.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Chameleon;
 
-public class Context : Microsoft.EntityFrameworkCore.DbContext
+public class Context : DbContext
 {
     public DbSet<User> User { get; set; }
     public DbSet<Roles> Roles { get; set; }
@@ -17,31 +17,37 @@ public class Context : Microsoft.EntityFrameworkCore.DbContext
     public DbSet<ContactDetails> ContactDetails { get; set; }
     public DbSet<UsersContactDetails> UsersContactDetails { get; set; }
 
+    public bool IsTesting { get; set; } = false;
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseMySql(
-            "server=localhost;user id=root;password=tshishi;database=Chameleons",
-            new MariaDbServerVersion(new Version(10, 5, 4)),
-            mySqlOptions => { }
-        );
-    }
-
-    protected void ConfigureForTesting(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseInMemoryDatabase("TestDatabase");
+        if (!optionsBuilder.IsConfigured)
+        {
+            if (IsTesting)
+            {
+                optionsBuilder.UseInMemoryDatabase("TestDatabase");
+            }
+            else
+            {
+                optionsBuilder.UseMySql(
+                    "server=localhost;user id=root;password=tshishi;database=Chameleons",
+                    new MariaDbServerVersion(new Version(10, 5, 4)),
+                    mySqlOptions => { }
+                );
+            }
+        }
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         // Config Entity
-        modelBuilder.ApplyConfiguration<Roles>(new RoleConfig());
-        modelBuilder.ApplyConfiguration<User>(new UserConfig());
-        modelBuilder.ApplyConfiguration<UsersRoles>(new UserRoleConfig());
-        modelBuilder.ApplyConfiguration<Locality>(new LocalityConfig());
-        modelBuilder.ApplyConfiguration<Country>(new CountryConfig());
-        modelBuilder.ApplyConfiguration<ContactDetails>(new ContactDetailsConfig());
-        modelBuilder.ApplyConfiguration<UsersContactDetails>(new UserContactDetailsConfig());
+        modelBuilder.ApplyConfiguration(new RoleConfig());
+        modelBuilder.ApplyConfiguration(new UserConfig());
+        modelBuilder.ApplyConfiguration(new UserRoleConfig());
+        modelBuilder.ApplyConfiguration(new LocalityConfig());
+        modelBuilder.ApplyConfiguration(new CountryConfig());
+        modelBuilder.ApplyConfiguration(new ContactDetailsConfig());
+        modelBuilder.ApplyConfiguration(new UserContactDetailsConfig());
 
         // Allimenter la DB
 
